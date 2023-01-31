69fde3df4ad5d832586ccc0edd1adf652f4ddd1c
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-31 12:00:10 +0600

    reload

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function setModal() {
     btnTag.innerHTML = 'restart'
     document.addEventListener('keydown', (event) => {
         if (event.isComposing || event.keyCode === 13) {
-            location.restart()
+            document.location.reload()
         }
     })
     div.append(textTag, btnTag, scoreTag)
